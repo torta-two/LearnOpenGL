b4d3d5b8fd3e067745fda21581288cdd0c5b9ea4
torta-two <616806129@qq.com>
2019-09-02 20:01:56 +0800

    add mouse control

--- Project/Main.cpp
+++ Project/Main.cpp
@@ -10,11 +10,29 @@ using namespace glm;
 const char* vertexPath = "shader.vertex";
 const char* fragmentPath = "shader.fragment";
 
-const unsigned int SCREEN_WIDTH = 512;
-const unsigned int SCREEN_HEIGHT = 512;
+const unsigned int SCREEN_WIDTH = 800;
+const unsigned int SCREEN_HEIGHT = 600;
+
+vec3 cameraPos = vec3(0, 0, 3);
+vec3 cameraFront = vec3(0, 0, -1);
+vec3 cameraUp = vec3(0, 1, 0);
+
+float deltaTime = 0.0f;	//当前帧与上一帧的时间差
+float lastFrame = 0.0f;	//上一帧deltaTime
+
+float lastX = SCREEN_WIDTH/2;
+float lastY = 300;
+const float sensitivity = 0.08f;
+
+float myPitch = 0.0f;
+float myYaw = -90.0f;
+
+float fov = 45.0f;
 
 unsigned int GetTexture(const char *imgPath, int index, GLenum warpMode, GLenum filterMode);
 mat4 CameraMove(vec3 &cameraPos, vec3 &cameraFront, vec3 &cameraUp);
+void mouse_callback(GLFWwindow* window, double xpos, double ypos);
+void scroll_callback(GLFWwindow* window, double xOffset, double yOffset);
 
 float vertices[] = {
 	-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
@@ -90,6 +108,10 @@ glm::vec3 cubePositions[] = {
 int main()
 {
 	GLFWwindow* window = InitOpenGL(SCREEN_WIDTH, SCREEN_HEIGHT);
+	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+	glfwSetCursorPosCallback(window, mouse_callback);
+	glfwSetScrollCallback(window, scroll_callback);
+
 	Shader shader(vertexPath, fragmentPath);
 
 	unsigned int VBO, VAO, EBO;
@@ -123,30 +145,23 @@ int main()
 	unsigned int texture1 = GetTexture("wall.jpg", 0, GL_REPEAT, GL_LINEAR);
 	unsigned int texture2 = GetTexture("face.jpg", 1, GL_REPEAT, GL_LINEAR);
 
-	vec3 cameraPos = vec3(0.0f, 0.0f, 3.0f);
-	vec3 cameraFront = vec3(0.0f, 0.0f, -1.0f);
-	vec3 cameraUp = vec3(0.0f, 1.0f, 0.0f);
+	shader.UseShader();
+	shader.SetInt("texture1", 0);
+	shader.SetInt("texture2", 1);
+
 
 	while (!glfwWindowShouldClose(window))
 	{
 		glClearColor(0.8f, 0.9f, 1.0f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-		
+		float currentFrame = (float)glfwGetTime();
+		deltaTime = currentFrame - lastFrame;
+		lastFrame = currentFrame;
+
 		//------------------------------------------------------------------
-
-		//glBindTexture(GL_TEXTURE_2D, texture1);
-		//glBindTexture(GL_TEXTURE_2D, texture2);
-
 		shader.UseShader();
-		
-		//glm::mat4 viewMatrix = glm::mat4(1.0f);
-		//viewMatrix = glm::translate(viewMatrix, glm::vec3(x, y, z));
 
-		glm::mat4 projMatrix = glm::mat4(1.0f);
-		//fov,aspeat,near,far
-		projMatrix = glm::perspective(glm::radians(45.0f), (float)SCREEN_WIDTH / (float)SCREEN_HEIGHT, 0.1f,100.0f);
-		
 		/*Gram-Schmidt Process:
 		===========================================================
 		vec3 cameraPos = vec3(0.0f, 0.0f, 3.0f);
@@ -158,21 +173,21 @@ int main()
 		up = normalize(cross(cameraDirection, right));		
 		===========================================================*/
 
-
-		mat4 view = CameraMove(cameraPos, cameraFront, cameraUp);
-
-		shader.SetMatrix("viewMatrix", view);
+		glm::mat4 projMatrix = glm::perspective(glm::radians(fov), (float)SCREEN_WIDTH / (float)SCREEN_HEIGHT, 0.1f, 100.0f);
 		shader.SetMatrix("projMatrix", projMatrix);
-		shader.SetInt("texture1", 0);
-		shader.SetInt("texture2", 1);
-		
+
+		mat4 viewMatrix = CameraMove(cameraPos, cameraFront, cameraUp);
+		shader.SetMatrix("viewMatrix", viewMatrix);
+		glBindVertexArray(VAO);
+
 		for (int i = 0; i < 10; i++)
 		{
 			glm::mat4 modelMatrix = glm::mat4(1.0f);
-			modelMatrix = glm::rotate(modelMatrix, glm::radians(45.0f * i), glm::vec3(0.0f, 1.0f, 0.0f));
 			modelMatrix = translate(modelMatrix, cubePositions[i]);
+			float angle = 20.0f * i;
+			modelMatrix = glm::rotate(modelMatrix, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
 			shader.SetMatrix("modelMatrix", modelMatrix);
-			glBindVertexArray(VAO);
+			
 			glDrawArrays(GL_TRIANGLES, 0, 36);
 		}
 
@@ -191,7 +206,9 @@ int main()
 
 mat4 CameraMove(vec3 &cameraPos, vec3 &cameraFront, vec3 &cameraUp)
 {
-	float speed = 0.08f;
+	mat4 matrix = mat4(1.0f);
+
+	float speed = 2.5f * deltaTime;
 
 	if (Input::GetKeyDown(GLFW_KEY_LEFT))
 		cameraPos -= speed * normalize(cross(cameraFront, cameraUp));
@@ -202,9 +219,52 @@ mat4 CameraMove(vec3 &cameraPos, vec3 &cameraFront, vec3 &cameraUp)
 	if (Input::GetKeyDown(GLFW_KEY_DOWN))
 		cameraPos -= speed * cameraFront;
 
-	return lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
+	matrix = lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
+
+	return matrix;
 }
 
+bool firstMouse = true;
+
+void mouse_callback(GLFWwindow* window, double xpos, double ypos)
+{
+	if (firstMouse)
+	{
+		lastX = (float)xpos;
+		lastY = (float)ypos;
+		firstMouse = false;
+	}
+
+	float yOffset = ((float)xpos - lastX) * sensitivity;
+	float xOffset = (lastY - (float)ypos) * sensitivity;
+	lastX = (float)xpos;
+	lastY = (float)ypos;
+
+	myPitch = myPitch + xOffset;
+	myYaw = myYaw + yOffset;
+
+	if (myPitch > 89.0f)
+		myPitch = 89.0f;
+	if (myPitch < -89.0f)
+		myPitch = -89.0f;
+
+	vec3 front;
+	front.x = cos(radians(myPitch)) * cos(radians(myYaw));
+	front.y = sin(radians(myPitch));
+	front.z = cos(radians(myPitch)) * sin(radians(myYaw)); 
+
+	cameraFront = normalize(front);
+}
+
+void scroll_callback(GLFWwindow* window, double xOffset, double yOffset)
+{
+	if (fov >= 1.0f && fov <= 65.0f)
+		fov -= (float)yOffset;
+	if (fov < 1.0f)
+		fov = 1.0f;
+	if (fov > 45.0f)
+		fov = 45.0f;
+}
 
 unsigned int GetTexture(const char *imgPath, int index, GLenum warpMode, GLenum filterMode)
 {
